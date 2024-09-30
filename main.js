a55b8992e7e37e38e54c700e5eda0e0641b05382
taraskunuts <117195745+taraskunuts@users.noreply.github.com>
2024-09-30 18:13:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+let age = "13"
+console.log(age)
+const name = "Taras"
+console.log(name)
+let isStudent = Boolean(false)
+console.log(isStudent)
+let myString = "Спочатку мрії здаються неможливими, потім неправдоподібними, а потім неминучими."
+console.log(myString)
+let myNumber = 3
+myNumber += 10
+console.log(myNumber)
+const myNull = null
+console.log(myNull)
+let userName = prompt("Будь ласка введіть ваше імя")
+console.log(userName)
+let userConfirmed = confirm("Ви підтверджуєте дію?")
+alert(userConfirmed ? "Дякую за підтвердження!" : "Дію відмінено!")     
+let accountDelete = confirm("Ви дійсно хочете видалити свій акаунт?")
+alert("Увага! Ця дія небезпечна.")
+let accountDeleting = confirm("Ви підтверджуєте цю дію?")
+alert(accountDeleting ? "Дякую за підтвердження!" : "Дію відмінено!")
+
+
